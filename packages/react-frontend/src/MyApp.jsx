d183673e9790d5a808dd61214b79c9de527c8e73
Arya Sastry <atsastry@calpoly.edu>
2025-04-06 20:58:57 -0700

    created a form to input data

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,26 +1,23 @@
 import React, { useState } from "react";
 import Table from "./Table";
+import Form from "./Form";
 
 function MyApp() {
-  const [characters, setCharacters] = useState([
-    { name: "Charlie", job: "Janitor" },
-    { name: "Mac", job: "Bouncer" },
-    { name: "Dee", job: "Aspiring actress" },
-    { name: "Dennis", job: "Bartender" }
-  ]);
+  const [characters, setCharacters] = useState([]);
 
   function removeOneCharacter(index) {
     const updated = characters.filter((_, i) => i !== index);
     setCharacters(updated);
   }
 
-  return (
-    <div className="container">
-      <Table
-        characterData={characters}
-        removeCharacter={removeOneCharacter}
-      />
-    </div>
+return (
+  <div className="container">
+    <Table
+      characterData={characters}
+      removeCharacter={removeOneCharacter}
+    />
+    <Form />
+  </div>
   );
 }
 
