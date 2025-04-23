3661a6d2533bf91149625baa8c40b893ddbe3c97
Arya Sastry <atsastry@calpoly.edu>
2025-04-22 19:01:43 -0700

    step 1 and 2 done: database access implemented

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import userService from "./user-services.js"
 
 const app = express();
 const port = 8000;
@@ -8,125 +9,53 @@ app.use(cors()); // allows backend to respond to calls coming from diff ports (f
  
 app.use(express.json());
 
-const users = {
-  users_list: [
-    {
-      id: "xyz789",
-      name: "Charlie",
-      job: "Janitor"
-    },
-    {
-      id: "abc123",
-      name: "Mac",
-      job: "Bouncer"
-    },
-    {
-      id: "ppp222",
-      name: "Mac",
-      job: "Professor"
-    },
-    {
-      id: "yat999",
-      name: "Dee",
-      job: "Aspiring actress"
-    },
-    {
-      id: "zap555",
-      name: "Dennis",
-      job: "Bartender"
-    }
-  ]
-};
-
-const findUserByName = (name) => {
-  return users["users_list"].filter(
-    (user) => user["name"] === name
-  );
-};
-
-const generateRandomId = () => {
-  return Math.random().toString(36).substring(2, 9);
-};
-
-const addUser = (user) => {
-  const newUser = {
-    ...user, 
-    id: generateRandomId()
-  };
-  users["users_list"].push(newUser);
-  return newUser;
-};
-
 app.get("/", (req, res) => {
   res.send("Hello World!");
 }); 
 
-const findUserById = (id) =>
-  users["users_list"].find((user) => user["id"] === id);
-
 app.get("/users/:id", (req, res) => {
-  const id = req.params["id"]; //or req.params.id
-  let result = findUserById(id);
-  if (result === undefined) {
-    res.status(404).send("Resource not found.");
-  } else {
-    res.send(result);
-  }
+  userService
+	.findUserById(req.params.id)
+	.then(user => {
+	  if (!user) return res.status(404).send("user not found");
+	  res.send(user);
+	})
+	.catch(err => res.status(500).send("error fetching user"));
 });
+
 
 app.get("/users", (req, res) => {
-  const name = req.query.name;
-  const job = req.query.job;
-
-  let filteredUsers = users.users_list;
-
-  if (name) {
-    filteredUsers = filteredUsers.filter(user => user.name === name);
-  }
-  
-  if (job) {
-    filteredUsers = filteredUsers.filter(user => user.job === job);
-  }
-  
-  res.send({ users_list: filteredUsers }); 
+  const { name, job } = req.query;  
+  userService
+	.getUsers(name, job)
+	.then(users => res.send({users_list: users }))
+	.catch(err => {
+	  console.error(err);
+	  res.status(500).send("error fetching users");
+	});  
 });
 
-// const addUser = (user) => {
-//  users["users_list"].push(user);
-//  return user;
-// };
-
-const deleteUserById = (id) => {
-  const userIndex = users.users_list.findIndex(user => user.id === id);
-  if (userIndex === -1) {
-    return false; // user not found
-  }
-  users.users_list.splice(userIndex, 1); // remove user
-  return true; // user deleted
-};
 
 app.post("/users", (req, res) => {
-  const userToAdd = req.body;
-  const addedUser = addUser(userToAdd);
-  res.status(201).json(addedUser); // sends back new user w generated ID 
+  userService
+	.addUser(req.body)
+	.then(newUser => res.status(201).send(newUser))
+	.catch(err => res.status(400).send("error adding user"));
 });
 
 app.delete("/users/:id", (req, res) => {
-  const id = req.params["id"];
-  const wasDeleted = deleteUserById(id);
-    
-  if (!wasDeleted) {
-    res.status(404).send("user not found.");
-  } else {
-    res.status(204).send(); // SUCCESS - no content
-  }
-});
+  userService
+	.deleteUserById(req.params.id)
+	.then(deleted => {
+	  if(!deleted) return res.status(404).send("user not found");
+	  res.status(204).send(); // no content, success ayeeee
+	})
+	.catch(err => {
+	  console.error(err);
+	  res.status(500).send("error deleting user");
+	});
+});  
 
-app.post("/users", (req, res) => {
-  const userToAdd = req.body;
-  const addedUser = addUser(userToAdd);
-  res.status(201).json(addedUser); // send back user with new ID
-});
 
 app.listen(port, () => {
   console.log(
---
+++ packages/express-backend/user-services.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+import userModel from "./models/user.js"
+import dotenv from 'dotenv';
+dotenv.config();
+
+// mongoose.set("debug", true);
+
+mongoose
+  .connect(process.env.MONGO_URI,
+  {    useNewUrlParser: true,
+       useUnifiedTopology: true,
+  })
+  .catch((error) => console.log(error));
+
+function getUsers(name, job) {
+  let promise;
+  if (name === undefined && job === undefined) {
+    promise = userModel.find();
+  } else if (name && !job) {
+    promise = findUserByName(name);
+  } else if (job && !name) {
+    promise = findUserByJob(job);
+  }
+  return promise;
+}
+
+function findUserById(id) {
+  return userModel.findById(id);
+}
+
+function addUser(user) {
+  const userToAdd = new userModel(user);
+  const promise = userToAdd.save();
+  return promise;
+}
+
+function findUserByName(name) {
+  return userModel.find({ name: name });
+}
+
+function findUserByJob(job) {
+  return userModel.find({ job: job });
+}
+
+function deleteUserById(id) {
+  return userModel.findByIdAndDelete(id);
+}
+
+export default {
+  addUser,
+  getUsers,
+  findUserById,
+  findUserByName,
+  findUserByJob,
+  deleteUserById,
+};
